0dfa7ca2d33185fce07448274b9eb752fc19c32e
khalifardy_miqdarsah <itdev.support22@tokiomarine-life.co.id>
2023-04-26 18:56:47 +0700

    terubah:         main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI, Response, File
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 from library_program.lib import Fuzifikasi, Defuzifikasi, inferensi
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/suggest_restoran")
